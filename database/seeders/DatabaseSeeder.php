21ed461d471e1cdc66bde360701972ea1c79c64e
Omidganji1380 <30316993+Omidganji1380@users.noreply.github.com>
2025-02-16 21:44:49 +0330

    docker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -58,8 +58,8 @@ class DatabaseSeeder extends Seeder
                 'active'      => true,
                 'country'     => 'United Arab Emirates',
                 'location'    => 'Dubai',
-                'countryFlag' => 'storage/openvpn/AE/A1383376600.jpg',
-                'configFile'  => 'storage/openvpn/AE/AE_vpn482908711.ovpn',
+                'countryFlag' => 'assets/openvpn/AE/A1383376600.jpg',
+                'configFile'  => 'assets/openvpn/AE/AE_vpn482908711.ovpn',
                 'type'        => 'ovpn'
             ],
             [
@@ -67,8 +67,8 @@ class DatabaseSeeder extends Seeder
                 'active'      => true,
                 'country'     => 'United Arab Emirates',
                 'location'    => 'Dubai',
-                'countryFlag' => 'storage/openvpn/AE/A1383376600.jpg',
-                'configFile'  => 'storage/openvpn/AE/AE_vpn482908711.ovpn',
+                'countryFlag' => 'assets/openvpn/AE/A1383376600.jpg',
+                'configFile'  => 'assets/openvpn/AE/AE_vpn482908711.ovpn',
                 'type'        => 'ovpn'
             ],
             [
@@ -76,8 +76,8 @@ class DatabaseSeeder extends Seeder
                 'active'      => true,
                 'country'     => 'United Arab Emirates',
                 'location'    => 'Dubai',
-                'countryFlag' => 'storage/openvpn/AE/A1383376600.jpg',
-                'configFile'  => 'storage/openvpn/AE/AE_wams.ovpn',
+                'countryFlag' => 'assets/openvpn/AE/A1383376600.jpg',
+                'configFile'  => 'assets/openvpn/AE/AE_wams.ovpn',
                 'type'        => 'ovpn'
             ],
             [
@@ -85,8 +85,8 @@ class DatabaseSeeder extends Seeder
                 'active'      => true,
                 'country'     => 'Australia',
                 'location'    => 'Sydney',
-                'countryFlag' => 'storage/openvpn/AU/150x90-australia-flag3-OPTIMIZED.jpg',
-                'configFile'  => 'storage/openvpn/AU/AU_vpn181866999.ovpn',
+                'countryFlag' => 'assets/openvpn/AU/150x90-australia-flag3-OPTIMIZED.jpg',
+                'configFile'  => 'assets/openvpn/AU/AU_vpn181866999.ovpn',
                 'type'        => 'ovpn'
             ],
             [
@@ -94,8 +94,8 @@ class DatabaseSeeder extends Seeder
                 'active'      => true,
                 'country'     => 'Canada',
                 'location'    => 'Ottawa',
-                'countryFlag' => 'storage/openvpn/CA/images.jpeg',
-                'configFile'  => 'storage/openvpn/CA/CA_vpn137292210.ovpn',
+                'countryFlag' => 'assets/openvpn/CA/images.jpeg',
+                'configFile'  => 'assets/openvpn/CA/CA_vpn137292210.ovpn',
                 'type'        => 'ovpn'
             ],
             [
@@ -103,8 +103,8 @@ class DatabaseSeeder extends Seeder
                 'active'      => true,
                 'country'     => 'Canada',
                 'location'    => 'Ottawa',
-                'countryFlag' => 'storage/openvpn/CA/images.jpeg',
-                'configFile'  => 'storage/openvpn/CA/CA_vpn748313328.ovpn',
+                'countryFlag' => 'assets/openvpn/CA/images.jpeg',
+                'configFile'  => 'assets/openvpn/CA/CA_vpn748313328.ovpn',
                 'type'        => 'ovpn'
             ],
             [
@@ -112,8 +112,8 @@ class DatabaseSeeder extends Seeder
                 'active'      => true,
                 'country'     => 'Germany',
                 'location'    => 'Berlin',
-                'countryFlag' => 'storage/openvpn/DE/Flag_of_Germany.svg.png',
-                'configFile'  => 'storage/openvpn/DE/DE_vpn408213436.ovpn',
+                'countryFlag' => 'assets/openvpn/DE/Flag_of_Germany.svg.png',
+                'configFile'  => 'assets/openvpn/DE/DE_vpn408213436.ovpn',
                 'type'        => 'ovpn'
             ],
             [
@@ -121,8 +121,8 @@ class DatabaseSeeder extends Seeder
                 'active'      => true,
                 'country'     => 'Germany',
                 'location'    => 'Berlin',
-                'countryFlag' => 'storage/openvpn/DE/Flag_of_Germany.svg.png',
-                'configFile'  => 'storage/openvpn/DE/DE_vpn836610036.ovpn',
+                'countryFlag' => 'assets/openvpn/DE/Flag_of_Germany.svg.png',
+                'configFile'  => 'assets/openvpn/DE/DE_vpn836610036.ovpn',
                 'type'        => 'ovpn'
             ],
             [
@@ -130,8 +130,8 @@ class DatabaseSeeder extends Seeder
                 'active'      => true,
                 'country'     => 'Germany',
                 'location'    => 'Berlin',
-                'countryFlag' => 'storage/openvpn/DE/Flag_of_Germany.svg.png',
-                'configFile'  => 'storage/openvpn/DE/DE_vpn779138305.ovpn',
+                'countryFlag' => 'assets/openvpn/DE/Flag_of_Germany.svg.png',
+                'configFile'  => 'assets/openvpn/DE/DE_vpn779138305.ovpn',
                 'type'        => 'ovpn'
             ],
             [
@@ -139,8 +139,8 @@ class DatabaseSeeder extends Seeder
                 'active'      => true,
                 'country'     => 'United Kingdom',
                 'location'    => 'London',
-                'countryFlag' => 'storage/openvpn/GB/Flag_of_the_United_Kingdom_(3-5).svg.png',
-                'configFile'  => 'storage/openvpn/GB/GB_vpn227320697.ovpn',
+                'countryFlag' => 'assets/openvpn/GB/Flag_of_the_United_Kingdom_(3-5).svg.png',
+                'configFile'  => 'assets/openvpn/GB/GB_vpn227320697.ovpn',
                 'type'        => 'ovpn'
             ],
             [
@@ -148,8 +148,8 @@ class DatabaseSeeder extends Seeder
                 'active'      => true,
                 'country'     => 'South Korea',
                 'location'    => 'Seoul',
-                'countryFlag' => 'storage/openvpn/KR/import-flag-south-korea1-150x90.jpg',
-                'configFile'  => 'storage/openvpn/KR/KR_vpn304549449.ovpn',
+                'countryFlag' => 'assets/openvpn/KR/import-flag-south-korea1-150x90.jpg',
+                'configFile'  => 'assets/openvpn/KR/KR_vpn304549449.ovpn',
                 'type'        => 'ovpn'
             ],
             [
@@ -157,8 +157,8 @@ class DatabaseSeeder extends Seeder
                 'active'      => true,
                 'country'     => 'Thailand',
                 'location'    => 'Bangkok',
-                'countryFlag' => 'storage/openvpn/TH/Flag_of_Costa_Rica.svg.png',
-                'configFile'  => 'storage/openvpn/TH/TH_vpn419711180.ovpn',
+                'countryFlag' => 'assets/openvpn/TH/Flag_of_Costa_Rica.svg.png',
+                'configFile'  => 'assets/openvpn/TH/TH_vpn419711180.ovpn',
                 'type'        => 'ovpn'
             ],
             [
@@ -166,8 +166,8 @@ class DatabaseSeeder extends Seeder
                 'active'      => true,
                 'country'     => 'United State',
                 'location'    => 'New York',
-                'countryFlag' => 'storage/openvpn/US/t_flagusa28s.jpg',
-                'configFile'  => 'storage/openvpn/US/US_vpn896793453.ovpn',
+                'countryFlag' => 'assets/openvpn/US/t_flagusa28s.jpg',
+                'configFile'  => 'assets/openvpn/US/US_vpn896793453.ovpn',
                 'type'        => 'ovpn'
             ],
             [
@@ -175,8 +175,8 @@ class DatabaseSeeder extends Seeder
                 'active'      => true,
                 'country'     => 'Japan',
                 'location'    => 'Tokyo',
-                'countryFlag' => 'storage/openvpn/JP/JP-flag.jpg',
-                'configFile'  => 'storage/openvpn/JP/JP_vpn706006471.ovpn',
+                'countryFlag' => 'assets/openvpn/JP/JP-flag.jpg',
+                'configFile'  => 'assets/openvpn/JP/JP_vpn706006471.ovpn',
                 'type'        => 'ovpn'
             ],
         ];
